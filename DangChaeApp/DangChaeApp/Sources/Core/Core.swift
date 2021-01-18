925076b6ae771b9cace23414831a2045c709f428
이동영 <o.o.wl.soft@gmail.com>
2021-01-19 08:51:08 +0900

    :package: 디렉터리 구조 정리

---
+++ DangChaeApp/DangChaeApp/Sources/Core/Core.swift
@@ -0,0 +1,13 @@
+//
+//  Core.swift
+//  DangChaeApp
+//
+//  Created by dongyoung.lee on 2021/01/19.
+//
+
+import Foundation
+
+// TODO: 삭제 예정🔮
+/// `파일 디렉터리 구조를 잡기위해 넣은 파일입니다.` (📝 아무 파일도 없는 디렉터리는 추적하지 않는 issue)
+/// 이 후 첫 파일이 생성되고 merge되면 삭제할 예정입니다.
+
