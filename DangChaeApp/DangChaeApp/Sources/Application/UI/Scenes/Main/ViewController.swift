6a6eaa441fc19312b48391e3463b9321666673f0
이동영(모바일) <o.o.wl.soft@gmail.com>
2021-01-19 08:52:28 +0900

    ⚙️ config/project (#1)
    * :see_no_evil:  .gitignore 설정
    
    * :tada: 프로젝트 생성
    
    * :package: 디렉터리 구조 정리

---
+++ DangChaeApp/DangChaeApp/Sources/Application/UI/Scenes/Main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DangChaeApp
+//
+//  Created by dongyoung.lee on 2021/01/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
