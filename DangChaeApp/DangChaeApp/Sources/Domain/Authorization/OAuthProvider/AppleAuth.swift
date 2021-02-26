7713db5cc45e7a9bcea8bb12867b923d6b7bc00c
이동영 <o.o.wl.soft@gmail.com>
2021-02-27 04:47:28 +0900

    :sparkles: 애플 로그인 구현 완료

--- DangChaeApp/DangChaeApp/Sources/Domain/Authorization/OAuthProvider/AppleAuth.swift
+++ DangChaeApp/DangChaeApp/Sources/Domain/Authorization/OAuthProvider/AppleAuth.swift
@@ -7,10 +7,24 @@
 
 import Foundation
 import RxSwift
-import KakaoSDKAuth
+import AuthenticationServices
 
 final class AppleAuth: OAuth {
+  
   func authorize() -> Observable<AuthResponse> {
-    return .empty()
+    let request = ASAuthorizationAppleIDProvider().createRequest()
+    let controller = ASAuthorizationController(authorizationRequests: [request])
+    controller.performRequests()
+    return controller.rx.accessTokenDidLoad
+      .map { accessToken in
+        guard let accessToken = accessToken else { throw Error.tokenDidNotFetched }
+        return AuthResponse(provider: .apple, accessToken: accessToken)
+      }
+  }
+  
+  enum Error: LocalizedError {
+    case tokenDidNotFetched
+    
+    var errorDescription: String? { "네이버와 연결을 실패했습니다." }
   }
 }
